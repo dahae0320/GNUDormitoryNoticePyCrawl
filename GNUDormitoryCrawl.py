8587a5f52e270bbb1d57bf0f96b8fefe0972804e
dahae0320 <ss7ssy@naver.com>
2020-11-29 22:15:33 +0900

    크롤링 데이터 집어넣기

--- GNUDormitoryCrawl.py
+++ GNUDormitoryCrawl.py
@@ -9,28 +9,30 @@ firebase_admin.initialize_app(cred, {
     'databaseURL' : 'https://gnudormitorynotice.firebaseio.com/'
 })
 
-# 공지사항 페이지 1
-for i in range(15):
-    num = i+1
-    data = requests.get('http://dorm.gnu.ac.kr/program/multipleboard/BoardList.jsp?groupNo=11171&searchType=&searchString=&category=&type=&cpage=1')
-    data.encoding = "UTF-8"
-    parser = BeautifulSoup(data.text, 'html.parser')    #, from_encoding='utf-8'
-    notice_num = parser.select("#body_content > form > div > div.board > table > tbody > tr.row"+ str(num) +"> td.first")[0].text
-    # print(num)
-    # print(notice_num.strip())
+# 공지사항 페이지별로 데이터
+def noticePageCrawler(number):
+    for i in range(15):
+        num = i+1
+        data = requests.get('http://dorm.gnu.ac.kr/program/multipleboard/BoardList.jsp?groupNo=11171&searchType=&searchString=&category=&type=&cpage='+str(number))
+        data.encoding = "UTF-8"
+        parser = BeautifulSoup(data.text, 'html.parser')    #, from_encoding='utf-8'
+        notice_num = parser.select("#body_content > form > div > div.board > table > tbody > tr.row"+ str(num) +"> td.first")[0].text
+        # print(num)
+        print(notice_num.strip())
 
-    detail_data = requests.get('http://dorm.gnu.ac.kr/program/multipleboard/BoardView.jsp?groupNo=11171&boardNo='+str(notice_num.strip()))
-    parser1 = BeautifulSoup(detail_data.text, 'html.parser')
-    title = parser1.select("#body_content > form > div.board > div.view > div.title > h3:not(span)")[0].text
-    description = parser1.select("#body_content > form > div.board > div.view > div.substance")[0].text
-    author = parser1.select("#body_content > form > div.board > div.view > div.info > dl.col4 > dd:nth-child(4)")[0].text
-    date = parser1.select("#body_content > form > div.board > div.view > div.info > dl.col4 > dd:nth-child(6)")[0].text
-    # print(date)
+        detail_data = requests.get('http://dorm.gnu.ac.kr/program/multipleboard/BoardView.jsp?groupNo=11171&boardNo='+str(notice_num.strip()))
+        parser1 = BeautifulSoup(detail_data.text, 'html.parser')
+        title = parser1.select("#body_content > form > div.board > div.view > div.title > h3:not(span)")[0].text
+        description = parser1.select("#body_content > form > div.board > div.view > div.substance")[0].text
+        author = parser1.select("#body_content > form > div.board > div.view > div.info > dl.col4 > dd:nth-child(4)")[0].text
+        date = parser1.select("#body_content > form > div.board > div.view > div.info > dl.col4 > dd:nth-child(6)")[0].text
+        print(title)
 
-    ref = db.reference(notice_num.strip())
-    ref.update( {'title' : title},
-                {'description' : description},
-                {'author' : author},
-                {'date' : date},
-                )
+        ref = db.reference(notice_num.strip())
+        ref.update({'title' : title})
+        ref.update({'description' : description})
+        ref.update({'author' : author})
+        ref.update({'date' : date})
 
+for i in range(8):
+    noticePageCrawler(i+1)
