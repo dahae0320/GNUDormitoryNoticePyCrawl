3de073a3f9e1a72bdd91830acb87b0507617c1d2
dahae0320 <ss7ssy@naver.com>
2020-11-30 12:26:13 +0900

    threading

--- Threading-time.py
+++ Threading-time.py
@@ -1,11 +1,15 @@
 import time
 import threading
+from GNUDormitoryCrawl import noticePageCrawler
 
 def thread_run():
     print('=====', time.ctime(), '=====')
     for i in range(1,3):
         #크롤링 코드
-        print('Tread running - ', i)
+        a = noticePageCrawler()
+        if a == 0:
+            print('새로 올라온 공지가 없습니다.')
+        print('Thread running - ', i)
     threading.Timer(5, thread_run).start() #5초마다 반복
 
 thread_run()
